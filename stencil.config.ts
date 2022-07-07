349b65b6dba78d23a755a8294eecaa02ae06a5c5
nopdam <niels.opdam@capgemini.com>
2022-07-07 16:24:29 +0200

    Initial commit

---
+++ stencil.config.ts
@@ -0,0 +1,28 @@
+import { Config } from '@stencil/core';
+import { angularOutputTarget } from '@stencil/angular-output-target';
+
+
+export const config: Config = {
+  namespace: 'cop-stencil',
+  outputTargets: [
+    {
+      type: 'dist',
+      esmLoaderPath: '../loader',
+    },
+    {
+      type: 'dist-custom-elements',
+    },
+    {
+      type: 'docs-readme',
+    },
+    {
+      type: 'www',
+      serviceWorker: null, // disable service workers
+    },
+    angularOutputTarget({
+      componentCorePackage: 'your-stencil-library-package-name',
+      directivesProxyFile: '../angular-workspace/projects/component-library/src/lib/stencil-generated/components.ts',
+      directivesArrayFile: '../angular-workspace/projects/component-library/src/lib/stencil-generated/index.ts',
+    })
+  ],
+};
